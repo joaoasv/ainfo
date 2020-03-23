ac25cb2820fd91e6ad7a2c32fbaa8b1d1e666aba
ofiliper <ofilipericardo@gmail.com>
2020-03-22 22:49:59 -0300

    remove inc folder

--- inc/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function conn(){
-    // $conn = mysqli_connect('localhost', 'root', '', 'ainfo');
-    $conn = mysqli_connect('sql164.main-hosting.eu', 'u533837016_asv', 'gatorade100', 'u533837016_ainfo');
-    return $conn;
-}
-
-?>
